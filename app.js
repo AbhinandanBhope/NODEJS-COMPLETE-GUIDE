c4f5577200ce5f88a04b53a3bd6ddd9faa8b1bca
Abhinandan Bhope <abhinandanbhope1642000@gmail.com>
2023-04-26 22:43:38 +0530

    Filtering Paths

--- app.js
+++ app.js
@@ -6,27 +6,19 @@ const bodyParser =require('body-parser');
 
 const express =require('express');
 const app =express();
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shope');
+
 app.use(bodyParser.urlencoded());
-
-app.use('/add-product',(req,res,next) =>{
-    console.log('in middleware');
-
-
-    res.send('<form action ="/product" method ="POST"><h1> Name of product </h1><input type ="text" name="title"><h1>Size of product</h1> <input type ="text" name="Size"><button type="submit">Add Product </button></form>');
-
-    
-    //next(); //allows the request to contains to the next middleware in line
-});
-app.use('/product',(req,res,next) =>{
-    console.log(req.body);
-    res.redirect('/');
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+app.use((req ,res ,next ) =>{
+  res.status(404).send('<h1>page is not found </h1>');
 
 });
-app.use('/',(req,res,next) => {
-    console.log('in middleware');
-    res.send('<h1>hellow form express.js!</h1>');
-});
-const server = http.createServer(app);
+
+
+
 
 
 
